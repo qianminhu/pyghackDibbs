5748e5862f64109921c7dcaa0b8824caaa7082fe
Ajith Antony <ajith.antony@gmail.com>
2017-09-23 22:41:38 +0000

    adds locactions, orgs, status

--- django/dibbs_app/admin.py
+++ django/dibbs_app/admin.py
@@ -5,6 +5,10 @@ from .models import *
 admin.site.register(Donation)
 admin.site.register(Item)
 admin.site.register(StorageRequirement)
+admin.site.register(DonationStatus)
+admin.site.register(DonationStatusLog)
+admin.site.register(Organization)
+admin.site.register(Location)
 
 
 # Register your models here.
--- django/dibbs_app/models.py
+++ django/dibbs_app/models.py
@@ -16,6 +16,26 @@ class Item(models.Model):
     def __str__(self):
         return "%s (%s)" % (self.name, self.quantity)
 
+class Location(models.Model):
+    name =  models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    address2 = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zipcode = models.CharField(max_length=10)
+    notes = models.TextField()
+    def __str__(self):
+        return self.name
+
+
+class Organization(models.Model):
+    name =  models.CharField(max_length=50)
+    tax_id = models.CharField(max_length=10)
+    locations = models.ManyToManyField(Location, verbose_name="locations")
+    def __str__(self):
+        return self.name
+
+
 
 class Donation(models.Model):
     title = models.CharField(max_length=50)
@@ -24,8 +44,20 @@ class Donation(models.Model):
     modify_time = models.DateTimeField(auto_now=True)
     deadline = models.DateTimeField(default=timezone.now)
     items = models.ManyToManyField(Item, verbose_name="list of items in this donation")
+    cost_estimate = models.DecimalField(decimal_places=2, max_digits=20)
     def __str__(self):
         return self.title
 
     
+class DonationStatus(models.Model):
+    status = models.CharField(max_length=50) 
+    def __str__(self):
+        return self.status
 
+class DonationStatusLog(models.Model):
+    donation =  models.ForeignKey(Donation)
+    status   =  models.ForeignKey(DonationStatus)
+    status_time = models.DateTimeField(auto_now=True)
+    notes = models.TextField()
+    def __str__(self):
+        return "%s - %s" % (donation.title, status.status)
