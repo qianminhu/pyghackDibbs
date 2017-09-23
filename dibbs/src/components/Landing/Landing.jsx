3dab01a5c88b46678122054701640bd494056efc
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-23 16:18:08 -0500

    Updated spacing

--- dibbs/src/components/Landing/Landing.jsx
+++ dibbs/src/components/Landing/Landing.jsx
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import "./Landing.css";
 
 class Landing extends Component {
   render() {
     return (
       <div class="Landing" >
         <div className="hero is-fullheight is-primary">
-          <nav className="level">
-            <Link to="/">
-              <span className="icon is-large">
-                <i className="fa fa-apple fa-2x"></i>
-              </span>
-            </Link>
-          </nav>
           <div className="hero-body">
             <div className="container">
-              <h1 className="title">Dibbs</h1>
-              <h2 className="subtitle">Save food. Do good.</h2>
-              <button className="button is-primary is-inverted is-outlined">Get Started</button>
+              <h1 className="title is-1">Dibbs</h1>
+              <h2 className="subtitle is-3">Save food. Do good.</h2>
+              <button className="button is-primary is-inverted is-outlined get-started">Get Started</button>
+              <button className="button is-primary is-inverted is-outlined sign-in">Sign In</button>
             </div>
           </div>
         </div>
--- dibbs/src/components/SignUp/SignUp.jsx
+++ dibbs/src/components/SignUp/SignUp.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+
+import NavBar from '../NavBar/NavBar.jsx';
 
 class SignUp extends Component {
-  render() {
-    return (
-      <div className="SignUp">
-        
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="SignUp">
+                <NavBar />
+                <div className="hero signup-hero">
+                    <div className="hero-body">
+                        <div className="field">
+                            <div className="label">Organization</div>
+                            <div className="control">
+                            <input type="text" className="input"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default SignUp;
