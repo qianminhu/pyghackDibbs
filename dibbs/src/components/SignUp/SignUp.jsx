f16a0c113fca75acd61b593b477a7481c676a747
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-23 15:34:44 -0500

    Added SignUp page

---
+++ dibbs/src/components/SignUp/SignUp.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div className="SignUp">
+        
+      </div>
+    );
+  }
+}
+
+export default SignUp;
