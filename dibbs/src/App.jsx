b06dd45a3224a988f9f7773939101d7f12c8d5d6
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-23 15:34:13 -0500

    Added routing

---
+++ dibbs/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Landing from './components/Landing/Landing.jsx';
+import SignUp from './components/SignUp/SignUp.jsx';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div className="Router">
+            <Route exact path="/" component={Landing} />
+            <Route path="/signup" component={SignUp} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
