35ea4a62229c9e028c9af16f5733d20824dcbe59
Ajith Antony <ajith.antony@gmail.com>
2017-09-23 21:28:50 +0000

    Model updates

--- django/dibbs_app/admin.py
+++ django/dibbs_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Donation
+from .models import *
 
 admin.site.register(Donation)
+admin.site.register(Item)
+admin.site.register(StorageRequirement)
+
 
 # Register your models here.
--- django/dibbs_app/models.py
+++ django/dibbs_app/models.py
@@ -1,8 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
+class StorageRequirement(models.Model):
+    name =  models.CharField(max_length=50)
+    details = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name =  models.CharField(max_length=50)
+    storage_requirement = models.ForeignKey(StorageRequirement)
+    quantity = models.CharField(max_length=50)
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.quantity)
+
 
 class Donation(models.Model):
-	title = models.CharField(max_length=50)
-	description = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    create_time = models.DateTimeField(auto_now_add=True)
+    modify_time = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField(default=timezone.now)
+    items = models.ManyToManyField(Item, verbose_name="list of items in this donation")
+    def __str__(self):
+        return self.title
+
+    
+
