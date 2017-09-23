6a9b376a9df6ae6a0e311f609d3b71f37e24b0d3
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-23 15:34:39 -0500

    Added Navigation Bar

---
+++ dibbs/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  render() {
+    return(
+      <div className="NavBar">
+        <nav className="navbar is-transparent">
+          <div className="navbar-start">
+            <a href="" className="navbar-item">Hello World</a>
+          </div>
+          <div className="navbar-end">
+            
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
