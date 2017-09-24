5429767113e2df0c6d8e26a9b48c223ea2180363
Steve Zazeski <steve@pixotech.com>
2017-09-23 20:58:04 -0500

    Add permissions to the REST api
    /donations can be read by anyone but logged in to write

--- django/dibbs_app/views.py
+++ django/dibbs_app/views.py
@@ -2,17 +2,22 @@ from django.contrib.auth.models import User, Group
 from dibbs_app.models import Donation
 from rest_framework import viewsets
 from dibbs_app.serializers import UserSerializer, GroupSerializer, DonationSerializer
+from rest_framework import permissions
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    permission_classes = (permissions.IsAdminUser,)
 
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = (permissions.IsAdminUser,)
 
 
 class DonationsViewSet(viewsets.ModelViewSet):
      queryset = Donation.objects.all()
      serializer_class = DonationSerializer
+     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
